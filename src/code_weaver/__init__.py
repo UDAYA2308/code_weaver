c7457513a9075ac9a37e45211908a6bbc1deb37a
udaya2308 <udaya14apr2000@gmail.com>
2026-05-09 15:30:13 -0400

    Remove __init__.py from .gitignore

---
+++ src/code_weaver/__init__.py
@@ -0,0 +1,1 @@
+# This package initializes the code_weaver agent.
---
+++ src/code_weaver/tools/__init__.py
@@ -0,0 +1,5 @@
+from .file_tools import file_tools
+from .web_tools import web_tools
+from .system_tools import system_tools
+
+all_tools = file_tools + web_tools + system_tools
